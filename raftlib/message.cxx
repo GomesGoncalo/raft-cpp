dd62dfd2e63d20c5169a1a6e126511eaf2186217
Goncalo Gomes <gomesgoncalo@gmail.com>
2024-07-09 08:29:46 +0100

    refactor(test): just moved some stuff around and made everythign compile

--- raftlib/message.cxx
+++ raftlib/message.cxx
@@ -7,69 +7,17 @@
 #include <boost/serialization/string.hpp>
 #include <boost/serialization/vector.hpp>
 
-struct SerializationProxy {
-  uint8_t tag;
-  RPCType rest;
-};
-
-namespace boost {
-namespace serialization {
-template <typename Archive>
-void save(Archive &ar, const boost::uuids::uuid &uuid,
-          const unsigned int version) {
-  ar &boost::serialization::make_array(uuid.data, uuid.size());
-}
-template <typename Archive>
-void save(Archive &ar, const log_entry &entry, const unsigned int version) {}
-template <typename Archive>
-void save(Archive &ar, const RequestVote &reqVote, const unsigned int version) {
-  ar &htonl(reqVote.term);
-  ar & reqVote.candidateId;
-  ar &htonl(reqVote.lastLogIndex);
-  ar &htonl(reqVote.lastLogTerm);
-}
-template <typename Archive>
-void save(Archive &ar, const RequestVoteResponse &reqVoteRes,
-          const unsigned int version) {
-  ar &htonl(reqVoteRes.term);
-  ar & reqVoteRes.voteGranted;
-}
-template <typename Archive>
-void save(Archive &ar, const AppendEntries &appendEntries,
-          const unsigned int version) {
-  ar &htonl(appendEntries.term);
-  ar & appendEntries.leaderId;
-  ar &htonl(appendEntries.prevLogIndex);
-  ar & appendEntries.entries;
-  ar &htonl(appendEntries.leaderCommit);
-}
-template <typename Archive>
-void save(Archive &ar, const AppendEntriesResponse &appendEntriesRes,
-          const unsigned int version) {
-  ar &htonl(appendEntriesRes.term);
-  ar & appendEntriesRes.success;
-}
-template <typename Archive>
-void save(Archive &ar, const SerializationProxy &proxy,
-          const unsigned int version) {
-  ar & proxy.tag;
-  std::visit([&ar](auto &&rest) { ar & rest; }, proxy.rest);
-}
-} // namespace serialization
-} // namespace boost
-BOOST_SERIALIZATION_SPLIT_FREE(AppendEntries)
-BOOST_SERIALIZATION_SPLIT_FREE(AppendEntriesResponse)
-BOOST_SERIALIZATION_SPLIT_FREE(RequestVote)
-BOOST_SERIALIZATION_SPLIT_FREE(RequestVoteResponse)
-BOOST_SERIALIZATION_SPLIT_FREE(SerializationProxy)
-
 static std::unique_ptr<asio::streambuf> serialize_internal(RPCType message) {
+  auto buf = std::make_unique<asio::streambuf>();
+
+  /*
   auto index = message.index();
   SerializationProxy proxy{static_cast<uint8_t>(index), std::move(message)};
-  auto buf = std::make_unique<asio::streambuf>();
   std::ostream os(&(*buf));
   boost::archive::binary_oarchive oa{os};
   oa << proxy;
+  */
+
   return buf;
 }
 
--- utils/constexpr.hxx
+++ utils/constexpr.hxx
@@ -2,7 +2,7 @@
 
 #include <type_traits>
 
-template <auto Start, auto End, auto Inc, class F>
+template <auto Start, auto End, auto Inc, typename F>
 constexpr void constexpr_for(F &&f) {
   if constexpr (Start < End) {
     f(std::integral_constant<decltype(Start), Start>());
